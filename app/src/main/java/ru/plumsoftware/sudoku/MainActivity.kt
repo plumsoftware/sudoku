41bfe98ee4ac1ceb2f3ca991ca35629ee15e2cdb
Slava <plumsoftwareofficial@gmail.com>
2024-11-16 12:36:35 +0600

    Либы [+] Добавил либы.

--- app/src/main/java/ru/plumsoftware/sudoku/MainActivity.kt
+++ app/src/main/java/ru/plumsoftware/sudoku/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ru.plumsoftware.sudoku.ui.theme.СудокуTheme
+import ru.plumsoftware.sudoku.ui.theme.SudokuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            СудокуTheme {
+            SudokuTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    СудокуTheme {
+    SudokuTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/ru/plumsoftware/sudoku/ui/theme/Theme.kt
+++ app/src/main/java/ru/plumsoftware/sudoku/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ru.plumsoftware.sudoku.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,10 +33,9 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun СудокуTheme(
+fun SudokuTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
